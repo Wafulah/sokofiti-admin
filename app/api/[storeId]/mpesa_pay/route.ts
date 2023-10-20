af734d42f5087575d819d58e0a049e0b6a824310
Wafulah <wafulahvictor@gmail.com>
2023-10-21 00:02:50 +0300

    mpesa

--- app/api/[storeId]/mpesa_pay/route.ts
+++ app/api/[storeId]/mpesa_pay/route.ts
@@ -63,7 +63,7 @@ export async function POST(
 
   try {
     // Call MpesaPay with the appropriate phone_number and amount
-    const session = MpesaPay("0710760872", amount);
+    MpesaPay("0710760872", amount);
     // If MpesaPay is successful, update the success URL
     const successUrl = `${process.env.FRONTEND_STORE_URL}/cart?success=1`;
     return NextResponse.json({ url: successUrl }, { headers: corsHeaders });
