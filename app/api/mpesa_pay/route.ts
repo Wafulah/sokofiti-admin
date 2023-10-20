d6ac94139ddb85cdcc2c60d537d66a88223264f1
Wafulah <wafulahvictor@gmail.com>
2023-10-20 18:33:22 +0300

    mpesa

--- app/api/mpesa_pay/route.ts
+++ app/api/mpesa_pay/route.ts
@@ -5,7 +5,7 @@ import MpesaPay from "@/lib/mpesa_lib";
 import prismadb from "@/lib/prismadb";
 
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": "https://sokofiti-store.vercel.app",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
