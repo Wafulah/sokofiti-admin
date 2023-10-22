6a12a7fceee5a790746d61d1250126b2d9a21680
Wafulah <wafulahvictor@gmail.com>
2023-10-22 03:51:05 +0300

    details

--- app/api/[storeId]/mpesa_pay/route.ts
+++ app/api/[storeId]/mpesa_pay/route.ts
@@ -18,7 +18,7 @@ export async function POST(
   req: Request,
   { params }: { params: { storeId: string } }
 ) {
-  const { productIds } = await req.json();
+  const { name, phoneNo, productIds } = await req.json();
 
   if (!productIds || productIds.length === 0) {
     return new NextResponse("Product ids are required", { status: 400 });
@@ -63,7 +63,7 @@ export async function POST(
 
   try {
     // Call MpesaPay with the appropriate phone_number and amount
-    const response = await MpesaPay("254710760872", amount);
+    const response = await MpesaPay(phoneNo, amount);
     // If MpesaPay is successful, update the success URL
     const successUrl = `${process.env.FRONTEND_STORE_URL}/cart?success=1`;
     return NextResponse.json({ url: successUrl }, { headers: corsHeaders });
