e2dcd7f5d9fd946d23a4d726f5d02f3ed43c6a91
Wafulah <wafulahvictor@gmail.com>
2023-10-25 01:04:56 +0300

    Redux

--- app/api/[storeId]/mpesa_pay/route.ts
+++ app/api/[storeId]/mpesa_pay/route.ts
@@ -31,56 +31,58 @@ export async function POST(
         productIds,
       },
     });
-  if (!productIds || productIds.length === 0) {
-    return new NextResponse("Product ids are required", { status: 400 });
-  }
 
-  const products = await prismadb.product.findMany({
-    where: {
-      id: {
-        in: productIds,
+    if (!productIds || productIds.length === 0) {
+      return new NextResponse("Product ids are required", { status: 400 });
+    }
+
+    const products = await prismadb.product.findMany({
+      where: {
+        id: {
+          in: productIds,
+        },
       },
-    },
-  });
-
-  const line_items = [];
-  let amount = 0;
-
-  products.forEach((product) => {
-    const unit_amount = product.price.toNumber();
-    const quantity = 1; // Set the quantity to 1
-    const amount_count = unit_amount * quantity; // Calculate the total amount
-    line_items.push({
-      amount: amount_count,
     });
-    amount += amount_count;
-  });
 
-  const order = await prismadb.order.create({
-    data: {
-      storeId: params.storeId,
-      isPaid: false,
-      orderItems: {
-        create: productIds.map((productId: string) => ({
-          product: {
-            connect: {
-              id: productId,
+    const line_items = [];
+    let amount = 0;
+
+    products.forEach((product) => {
+      const unit_amount = product.price.toNumber();
+      const quantity = 1; // Set the quantity to 1
+      const amount_count = unit_amount * quantity; // Calculate the total amount
+      line_items.push({
+        amount: amount_count,
+      });
+      amount += amount_count;
+    });
+
+    const order = await prismadb.order.create({
+      data: {
+        storeId: params.storeId,
+        isPaid: false,
+        orderItems: {
+          create: productIds.map((productId: string) => ({
+            product: {
+              connect: {
+                id: productId,
+              },
             },
-          },
-        })),
+          }),
+        },
       },
-    },
-  });
+    });
 
-  try {
-    // Call MpesaPay with the appropriate phone_number and amount
-    const response = await MpesaPay(phoneNo, amount);
-    // If MpesaPay is successful, update the success URL
-    const successUrl = `${process.env.FRONTEND_STORE_URL}/cart?success=1`;
-    return NextResponse.json({ url: successUrl }, { headers: corsHeaders });
-  } catch (error) {
-    // If there's an error, update the cancel URL
-    const cancelUrl = `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`;
-    return NextResponse.json({ url: cancelUrl }, { headers: corsHeaders });
+    try {
+      // Call MpesaPay with the appropriate phone_number and amount
+      const response = await MpesaPay(phoneNo, amount);
+      // If MpesaPay is successful, update the success URL
+      const successUrl = `${process.env.FRONTEND_STORE_URL}/cart?success=1`;
+      return NextResponse.json({ url: successUrl }, { headers: corsHeaders });
+    } catch (error) {
+      // If there's an error, update the cancel URL
+      const cancelUrl = `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`;
+      return NextResponse.json({ url: cancelUrl }, { headers: corsHeaders });
+    }
   }
 }
