704dd7d8ddcb2203020318331522b3465043b28f
Wafulah <wafulahvictor@gmail.com>
2023-10-31 01:13:03 +0300

    stores

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -1,11 +1,9 @@
-import { NextResponse } from 'next/server';
-import { auth } from '@clerk/nextjs';
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
 
-import prismadb from '@/lib/prismadb';
+import prismadb from "@/lib/prismadb";
 
-export async function POST(
-  req: Request,
-) {
+export async function POST(req: Request) {
   try {
     const { userId } = auth();
     const body = await req.json();
@@ -24,25 +22,52 @@ export async function POST(
       data: {
         name,
         userId,
-      }
+      },
     });
-  
+
     return NextResponse.json(store);
   } catch (error) {
-    console.log('[STORES_POST]', error);
+    console.log("[STORES_POST]", error);
     return new NextResponse("Internal error", { status: 500 });
   }
-};
+}
 
-export async function GET(req: Request) {
+export async function GET() {
   try {
     // Parse query parameters for bounds (southwest and northeast)
-    const { searchParams } = new URL(req.url);
+    const searchParams = {
+      swLat: "-1.0",
+      swLng: "2.0",
+      neLat: "3.0",
+      neLng: "4.0",
+    };
 
+    const swLat = parseFloat(searchParams.get("swLat") || "0");
+    const swLng = parseFloat(searchParams.get("swLng") || "0");
+    const neLat = parseFloat(searchParams.get("neLat") || "0");
+    const neLng = parseFloat(searchParams.get("neLng") || "0");
 
-    return NextResponse.json(searchParams);
+    if (isNaN(swLat) || isNaN(swLng) || isNaN(neLat) || isNaN(neLng)) {
+      return new NextResponse("Invalid bounds", { status: 400 });
+    }
+
+    // Fetch stores within the specified boundary
+    const stores = await prismadb.store.findMany({
+      where: {
+        latitude: {
+          gte: swLat, // Greater than or equal to southwest latitude
+          lte: neLat, // Less than or equal to northeast latitude
+        },
+        longitude: {
+          gte: swLng, // Greater than or equal to southwest longitude
+          lte: neLng, // Less than or equal to northeast longitude
+        },
+      },
+    });
+
+    return NextResponse.json(stores);
   } catch (error) {
-    console.error('[STORES_GET]', error);
+    console.error("[STORES_GET]", error);
     return new NextResponse("Internal error", { status: 500 });
   }
 }
