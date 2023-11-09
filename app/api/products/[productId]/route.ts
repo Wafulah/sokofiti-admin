6fcb1bd58f6066ff46b78b733bcc177bce9a1eac
Wafulah <wafulahvictor@gmail.com>
2023-11-09 23:49:20 +0300

    params

--- app/api/products/[productId]/route.ts
+++ app/api/products/[productId]/route.ts
@@ -3,6 +3,16 @@ import { auth } from "@clerk/nextjs";
 
 import prismadb from "@/lib/prismadb";
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export async function OPTIONS(req: Request, res: Response) {
+  return new NextResponse(null, { headers: corsHeaders });
+}
+
 export async function GET(
   req: Request,
   { params }: { params: { productId: string } }
@@ -24,9 +34,9 @@ export async function GET(
       }
     });
   
-    return NextResponse.json(product);
+    return NextResponse.json(product, { headers: corsHeaders });
   } catch (error) {
-    console.log('[PRODUCT_GET]', error);
+    console.log('[All_SPECIFIC_PRODUCT_GET]', error);
     return new NextResponse("Internal error", { status: 500 });
   }
 };
