586f91ba0e1c3c5361011836339488629ee31802
Wafulah <wafulahvictor@gmail.com>
2023-12-05 03:49:11 +0300

    api

--- app/admin/[storeId]/(routes)/new-county/components/county-form.tsx
+++ app/admin/[storeId]/(routes)/new-county/components/county-form.tsx
@@ -1,17 +1,17 @@
-"use client"
+"use client";
 
-import * as z from "zod"
-import axios from "axios"
-import { useState } from "react"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { toast } from "react-hot-toast"
+import * as z from "zod";
+import axios from "axios";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
 import { LuTrash2 as Trash } from "react-icons/lu";
-import { County } from "@prisma/client"
-import { useParams, useRouter } from "next/navigation"
+import { County } from "@prisma/client";
+import { useParams, useRouter } from "next/navigation";
 
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -19,54 +19,56 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Separator } from "@/components/ui/separator"
-import { Heading } from "@/components/ui/heading"
-import { AlertModal } from "@/components/modals/alert-modal"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+} from "@/components/ui/form";
+import { Separator } from "@/components/ui/separator";
+import { Heading } from "@/components/ui/heading";
+import { AlertModal } from "@/components/modals/alert-modal";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 const formSchema = z.object({
   name: z.string().min(2),
 });
 
-type CountyFormValues = z.infer<typeof formSchema>
+type CountyFormValues = z.infer<typeof formSchema>;
 
-interface CountyFormProps {
- 
-};
+interface CountyFormProps {}
 
-export const CountyForm: React.FC<CountyFormProps> = ({
-  
-}) => {
+export const CountyForm: React.FC<CountyFormProps> = ({}) => {
   const params = useParams();
   const router = useRouter();
 
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const title = 'Create county';
-  const description =  'Add a new county';
-  const toastMessage = 'County created.';
-  const action =  'Create';
+  const title = "Create county";
+  const description = "Add a new county";
+  const toastMessage = "County created.";
+  const action = "Create";
 
   const form = useForm<CountyFormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      name: '',
-    }
+      name: "",
+    },
   });
 
   const onSubmit = async (data: CountyFormValues) => {
     try {
       setLoading(true);
-      
-        await axios.post(`/api/${params.storeId}/counties`, data);
-     
+
+      await axios.post(`/api/${params.storeId}/counties`, data);
+
       router.refresh();
-      router.push(`admin/${params.storeId}/counties`);
+      router.push(`/admin/${params.storeId}/counties`);
       toast.success(toastMessage);
     } catch (error: any) {
-      toast.error('Something went wrong.');
+      toast.error("Something went wrong.");
     } finally {
       setLoading(false);
     }
@@ -78,30 +80,32 @@ export const CountyForm: React.FC<CountyFormProps> = ({
       await axios.delete(`/api/${params.storeId}/counties/${params.countyId}`);
       router.refresh();
       router.push(`/${params.storeId}/counties`);
-      toast.success('County deleted.');
+      toast.success("County deleted.");
     } catch (error: any) {
-      toast.error('Make sure you removed all Stores using this county first.');
+      toast.error("Make sure you removed all Stores using this county first.");
     } finally {
       setLoading(false);
       setOpen(false);
     }
-  }
+  };
 
   return (
     <>
-    <AlertModal 
-      isOpen={open} 
-      onClose={() => setOpen(false)}
-      onConfirm={onDelete}
-      loading={loading}
-    />
-     <div className="flex items-center justify-between">
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
         <Heading title={title} description={description} />
-       
       </div>
       <Separator />
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
           <div className="md:grid md:grid-cols-3 gap-8">
             <FormField
               control={form.control}
@@ -110,13 +114,16 @@ export const CountyForm: React.FC<CountyFormProps> = ({
                 <FormItem>
                   <FormLabel>Name</FormLabel>
                   <FormControl>
-                    <Input disabled={loading} placeholder="County name" {...field} />
+                    <Input
+                      disabled={loading}
+                      placeholder="County name"
+                      {...field}
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
-            
           </div>
           <Button disabled={loading} className="ml-auto" type="submit">
             {action}
